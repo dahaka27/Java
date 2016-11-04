090833348fc7188501ff5ddcadb68a48890dc43b
Loren Aparicio <loren.djkiller@gmail.com>
2016-11-04 21:06:57 +0100

    Add files via upload

---
+++ Unidad3Ejercicio14/src/unidad3ejercicio14/Unidad3Ejercicio14.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidad3ejercicio14;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author loren
+ */
+public class Unidad3Ejercicio14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner tc=new Scanner(System.in);
+    }
+    
+    public static int pedirMes(int n){
+    
+    while (n > 1 && n <=12);
+    return n;  
+    }
+    public static int pedirAnio(int a,int l1, int l2){
+    
+    while (a >=l1 && a <=l2);
+    return a;  
+    }
+    public static int pedirDia(int l1, int l2){
+    
+    while (n > 1 && n <=12);
+    return n;  
+    }
+    public static int mostrarSiguienteDia(int l1, int l2){
+    
+    while (n > 1 && n <=12);
+    return n;  
+    }
+    
+}
